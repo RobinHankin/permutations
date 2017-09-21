1d6152b4888ba3ada46bce15cad53d4cb3b35005
RobinHankin <hankin.robin@gmail.com>
2017-09-22 11:59:46 +1200

    kickoff, gradually transferring all packages to github.

---
+++ data/megaminx.R
@@ -0,0 +1,129 @@
+`megaminx` <-
+structure(list(White = list(c(10, 12, 14, 16, 18), c(11, 13, 
+15, 17, 19), c(21, 33, 45, 57, 69), c(22, 34, 46, 58, 60), c(23, 
+35, 47, 59, 61)), Purple = list(c(15, 67, 91, 81, 35), c(16, 
+68, 92, 82, 36), c(17, 69, 93, 83, 37), c(20, 22, 24, 26, 28), 
+    c(21, 23, 25, 27, 29)), DarkYellow = list(c(17, 29, 89, 79, 
+47), c(18, 20, 80, 70, 48), c(19, 21, 81, 71, 49), c(30, 32, 
+34, 36, 38), c(31, 33, 35, 37, 39)), DarkBlue = list(c(10, 32, 
+78, 118, 50), c(11, 33, 79, 119, 51), c(19, 31, 77, 117, 59), 
+    c(40, 42, 44, 46, 48), c(41, 43, 45, 47, 49)), Red = list(
+    c(11, 43, 115, 105, 61), c(12, 44, 116, 106, 62), c(13, 45, 
+    117, 107, 63), c(50, 52, 54, 56, 58), c(51, 53, 55, 57, 59
+    )), DarkGreen = list(c(13, 55, 103, 93, 23), c(14, 56, 104, 
+94, 24), c(15, 57, 105, 95, 25), c(60, 62, 64, 66, 68), c(61, 
+63, 65, 67, 69)), LightGreen = list(c(30, 88, 120, 110, 40), 
+    c(31, 89, 121, 111, 41), c(39, 87, 129, 119, 49), c(70, 72, 
+    74, 76, 78), c(71, 73, 75, 77, 79)), Orange = list(c(27, 
+99, 121, 71, 37), c(28, 90, 122, 72, 38), c(29, 91, 123, 73, 
+39), c(80, 82, 84, 86, 88), c(81, 83, 85, 87, 89)), LightBlue = list(
+    c(25, 65, 101, 123, 83), c(26, 66, 102, 124, 84), c(27, 67, 
+    103, 125, 85), c(90, 92, 94, 96, 98), c(91, 93, 95, 97, 99
+    )), LightYellow = list(c(53, 113, 125, 95, 63), c(54, 114, 
+126, 96, 64), c(55, 115, 127, 97, 65), c(100, 102, 104, 106, 
+108), c(101, 103, 105, 107, 109)), Pink = list(c(41, 75, 127, 
+107, 51), c(42, 76, 128, 108, 52), c(43, 77, 129, 109, 53), c(110, 
+112, 114, 116, 118), c(111, 113, 115, 117, 119)), Grey = list(
+    c(73, 85, 97, 109, 111), c(74, 86, 98, 100, 112), c(75, 87, 
+    99, 101, 113), c(120, 122, 124, 126, 128), c(121, 123, 125, 
+    127, 129))), class = c("permutation", "cycle"), .Names = c("White", 
+"Purple", "DarkYellow", "DarkBlue", "Red", "DarkGreen", "LightGreen", 
+"Orange", "LightBlue", "LightYellow", "Pink", "Grey"))
+
+`megaminx_colours` <-
+structure(c("Black", "Black", "Black", "Black", "Black", "Black", 
+"Black", "Black", "Black", "White", "White", "White", "White", 
+"White", "White", "White", "White", "White", "White", "Purple", 
+"Purple", "Purple", "Purple", "Purple", "Purple", "Purple", "Purple", 
+"Purple", "Purple", "DarkYellow", "DarkYellow", "DarkYellow", 
+"DarkYellow", "DarkYellow", "DarkYellow", "DarkYellow", "DarkYellow", 
+"DarkYellow", "DarkYellow", "DarkBlue", "DarkBlue", "DarkBlue", 
+"DarkBlue", "DarkBlue", "DarkBlue", "DarkBlue", "DarkBlue", "DarkBlue", 
+"DarkBlue", "Red", "Red", "Red", "Red", "Red", "Red", "Red", 
+"Red", "Red", "Red", "DarkGreen", "DarkGreen", "DarkGreen", "DarkGreen", 
+"DarkGreen", "DarkGreen", "DarkGreen", "DarkGreen", "DarkGreen", 
+"DarkGreen", "LightGreen", "LightGreen", "LightGreen", "LightGreen", 
+"LightGreen", "LightGreen", "LightGreen", "LightGreen", "LightGreen", 
+"LightGreen", "Orange", "Orange", "Orange", "Orange", "Orange", 
+"Orange", "Orange", "Orange", "Orange", "Orange", "LightBlue", 
+"LightBlue", "LightBlue", "LightBlue", "LightBlue", "LightBlue", 
+"LightBlue", "LightBlue", "LightBlue", "LightBlue", "LightYellow", 
+"LightYellow", "LightYellow", "LightYellow", "LightYellow", "LightYellow", 
+"LightYellow", "LightYellow", "LightYellow", "LightYellow", "Grey", 
+"Grey", "Grey", "Grey", "Grey", "Grey", "Grey", "Grey", "Grey", 
+"Grey", "Grey", "Grey", "Grey", "Grey", "Grey", "Grey", "Grey", 
+"Grey", "Grey", "Grey"), class = "noquote")
+
+
+`W` <-
+structure(list(White = list(c(10, 12, 14, 16, 18), c(11, 13, 
+15, 17, 19), c(21, 33, 45, 57, 69), c(22, 34, 46, 58, 60), c(23, 
+35, 47, 59, 61))), .Names = "White", class = c("permutation", 
+"cycle"))
+
+`Pu` <-
+structure(list(Purple = list(c(15, 67, 91, 81, 35), c(16, 68, 
+92, 82, 36), c(17, 69, 93, 83, 37), c(20, 22, 24, 26, 28), c(21, 
+23, 25, 27, 29))), .Names = "Purple", class = c("permutation", 
+"cycle"))
+
+`DY` <-
+structure(list(DarkYellow = list(c(17, 29, 89, 79, 47), c(18, 
+20, 80, 70, 48), c(19, 21, 81, 71, 49), c(30, 32, 34, 36, 38), 
+    c(31, 33, 35, 37, 39))), .Names = "DarkYellow", class = c("permutation", 
+"cycle"))
+
+`DB` <- 
+structure(list(DarkBlue = list(c(10, 32, 78, 118, 50), c(11, 
+33, 79, 119, 51), c(19, 31, 77, 117, 59), c(40, 42, 44, 46, 48
+), c(41, 43, 45, 47, 49))), .Names = "DarkBlue", class = c("permutation", 
+"cycle"))
+
+`R` <- 
+structure(list(Red = list(c(11, 43, 115, 105, 61), c(12, 44, 
+116, 106, 62), c(13, 45, 117, 107, 63), c(50, 52, 54, 56, 58), 
+    c(51, 53, 55, 57, 59))), .Names = "Red", class = c("permutation", 
+"cycle"))
+
+`DG` <- 
+structure(list(DarkGreen = list(c(13, 55, 103, 93, 23), c(14, 
+56, 104, 94, 24), c(15, 57, 105, 95, 25), c(60, 62, 64, 66, 68
+), c(61, 63, 65, 67, 69))), .Names = "DarkGreen", class = c("permutation", 
+"cycle"))
+
+`LG` <-
+structure(list(LightGreen = list(c(30, 88, 120, 110, 40), c(31, 
+89, 121, 111, 41), c(39, 87, 129, 119, 49), c(70, 72, 74, 76, 
+78), c(71, 73, 75, 77, 79))), .Names = "LightGreen", class = c("permutation", 
+"cycle"))
+
+`O` <- 
+structure(list(Orange = list(c(27, 99, 121, 71, 37), c(28, 90, 
+122, 72, 38), c(29, 91, 123, 73, 39), c(80, 82, 84, 86, 88), 
+    c(81, 83, 85, 87, 89))), .Names = "Orange", class = c("permutation", 
+"cycle"))
+
+`LB` <-
+structure(list(LightBlue = list(c(25, 65, 101, 123, 83), c(26, 
+66, 102, 124, 84), c(27, 67, 103, 125, 85), c(90, 92, 94, 96, 
+98), c(91, 93, 95, 97, 99))), .Names = "LightBlue", class = c("permutation", 
+"cycle"))
+
+`LY` <-
+structure(list(LightYellow = list(c(53, 113, 125, 95, 63), c(54, 
+114, 126, 96, 64), c(55, 115, 127, 97, 65), c(100, 102, 104, 
+106, 108), c(101, 103, 105, 107, 109))), .Names = "LightYellow", class = c("permutation", 
+"cycle"))
+
+`Pi` <- 
+structure(list(Pink = list(c(41, 75, 127, 107, 51), c(42, 76, 
+128, 108, 52), c(43, 77, 129, 109, 53), c(110, 112, 114, 116, 
+118), c(111, 113, 115, 117, 119))), .Names = "Pink", class = c("permutation", 
+"cycle"))
+
+`Gy` <-
+structure(list(Grey = list(c(73, 85, 97, 109, 111), c(74, 86, 
+98, 100, 112), c(75, 87, 99, 101, 113), c(120, 122, 124, 126, 
+128), c(121, 123, 125, 127, 129))), .Names = "Grey", class = c("permutation",
+"cycle"))
+
