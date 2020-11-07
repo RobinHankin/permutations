d12c16d1e444d619b65c85d4c7d60e85dfac267c
RobinHankin <hankin.robin@gmail.com>
2020-11-08 10:09:47 +1300

    kilominx functionality

--- inst/code.R
+++ inst/code.R
@@ -14,8 +14,16 @@
 # The last few lines (which create the superflip) appear in
 # jeremy_clark.txt and indeed megaminx.Rd in the package.
 
+# This file may easily be modified to create the kilominx group
+# instead of the megaminx group.  Function f() reads file 'data.txt'
+# but by commenting out two lines we effectively ignore the edges
+# [which have even-numbered facets] and create the kilominx group
+# instead.  If you do this, it might be a good idea to change the
+# filename from "megaminx.py" to "kilominx.py"; also look at the
+# commented lines at the bottom which distinguish kilo- from mega-.
 
-filename <- "megaminx.py"
+  filename <- "megaminx.py"
+# filename <- "kilominx.py"
 
 write(paste("# This file is not intended to be human-readable; it is readable by sage.  It is created by executing 'code.R'"),
             file=filename, append=FALSE)
@@ -41,10 +49,10 @@ putcommas <- function(s){
 f <- function(x){
   paste(c(
           opensquare,
-          open,putcommas(x[ 1: 5]),close,comma,
+          open,putcommas(x[ 1: 5]),close,comma,  # comment out this line for kilominx
           open,putcommas(x[ 6:10]),close,comma,
           open,putcommas(x[11:15]),close,comma,
-          open,putcommas(x[16:20]),close,comma,
+          open,putcommas(x[16:20]),close,comma,  # comment out this line for kilominx
           open,putcommas(x[21:25]),close,
           closesquare),collapse="")
 }
@@ -84,12 +92,13 @@ a11 = c11.inverse()
 a12 = c12.inverse()
 ",file=filename,append=TRUE)
 
-write("megaminx = PermutationGroup([c01,c02,c03,c04,c05,c06,c07,c08,c09,c10,c11,c12])",file=filename,append=TRUE)
+  write("megaminx = PermutationGroup([c01,c02,c03,c04,c05,c06,c07,c08,c09,c10,c11,c12])",file=filename,append=TRUE)
+# write("kilominx = PermutationGroup([c01,c02,c03,c04,c05,c06,c07,c08,c09,c10,c11,c12])",file=filename,append=TRUE)
 
 
+  write("superflip = megaminx.center().gens()[0]",file=filename,append=TRUE)
+# write("superflip = kilominx.center().gens()[0]",file=filename,append=TRUE)  # kilominx has trivial center
 
-
-write("superflip = megaminx.center().gens()[0]",file=filename,append=TRUE)
 write("big = c01*c02*c03*c04*c05*c06*c07*c08*c09*c10*c11*c12",file=filename,append=TRUE) # moves every facet
 write("(a06*a09*c04*c07)*(c01*a02*c01*c02^2*a03^2)^6*(a07*a04*c09*c06)*(a12^2*c09*a07*a11*a10)^9",file=filename,append=TRUE)
 
