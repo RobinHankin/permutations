535fe1c3d53070b15c0b6970fefd727b5101af25
Robin Hankin <hankin.robin@gmail.com>
2022-07-15 20:43:25 +1200

    new argument

--- R/faro.R
+++ R/faro.R
@@ -6,12 +6,14 @@
   }
 }
 
-`faro_gen` <- function(n,m,p1=id,p2=id){  # generalized Faro shuffle
+`faro_gen` <- function(n,m,p1=id,p2=id,interleave=TRUE){# generalized Faro shuffle
   M <- matrix(seq_len(n*m),n,m)
   p1 <- as.word(p1)
   p2 <- as.word(p2)
   size(p1) <- n
   size(p2) <- m
-  as.word(c(t(M[as.function(p1)(seq_len(n)),as.function(p2)(seq_len(m))])))
+  M <- M[as.function(p1)(seq_len(n)),as.function(p2)(seq_len(m))]
+  if(interleave){M <- t(M)}
+  as.word(c(M))
 }
 
