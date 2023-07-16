9031665b0895db594f90beec59fa64fd58bf6eb5
Robin Hankin <hankin.robin@gmail.com>
2023-07-16 15:18:38 +1200

    document all_cycle() stuff, as per issue #40

---
+++ R/all_perms_given_shape.R
@@ -0,0 +1,30 @@
+`allpermslike` <- function(o){ #   allpermslike(as.cycle("(1,2)(3,4)(5,6,7,8)"))
+  x <- lapply(unclass(as.cycle(o))[[1]],FUN = allcyc)
+  xlen <- unlist(lapply(x,length))
+  xseq <- sapply(xlen,seq_len,simplify=FALSE)
+  e <- as.matrix(expand.grid(xseq))
+  out <- nullcycle
+  for(i in seq_len(nrow(e))){
+    jj <- id
+    for(j in seq_len(ncol(e))){
+      jj <- jj + x[[j]][e[i,j]]
+    }
+    out <- c(out,jj)
+  }
+  return(out)
+}
+
+`some_perms_shape` <- function(shape){  # some_perms_shape(c(2,2,4))
+  M <- listParts(shape)
+  do.call("c",lapply(seq_along(M),function(i){as.cycle(unclass(M[[i]]))}))
+}
+
+`all_cyclic_shuffles` <- function(o){  # all_cyclic_shuffles(cyc_len(3:5))
+  o <- as.cycle(o)
+  do.call("c",lapply(seq_along(o),function(i){allpermslike(o[i])}))
+}
+
+`all_perms_shape` <- function(shape){# all_perms_shape(c(2,2,3))
+    all_cyclic_shuffles(some_perms_shape(shape))
+}
+
