68454c4c14d5949c93e9d31e4715ee8e9907af38
Robin Hankin <hankin.robin@gmail.com>
2023-07-28 08:31:47 +1200

    rationalise inline documentation (mostly remove it)

--- R/permfuns.R
+++ R/permfuns.R
@@ -1,47 +1,28 @@
-## There are only two functions that set the class of an object:
-## permutation(), which uses class(P) <- "permutation", and cycle(),
-## which uses class(x) <- "cycle".
-
-
-## a *cyclist* is a list of cycles: list(1:4,8:9) is a cyclist; but
-## this is informal.  The cycles are notionally distinct.
-
-## a *cycle* object is a list of cyclists.
-
-
 "word" <- function(M){
-    ## takes a matrix and returns a word object; silently coerces to
-    ## integer first
     stopifnot(is.matrix(M))
     storage.mode(M) <- "integer"
     if(nrow(M)>0){
       stopifnot(all(apply(M,1, singleword_valid)))
     }
-        class(M) <- c("permutation", "word")  # this is the *only*
-                                              # time an object is
-                                              # coerced to class
-                                              # permutation or word.
+        class(M) <- c("permutation", "word")   # NB this is the 
+                                               # *only* place that
+                                               # class "word" is
+                                               # assigned to an
+                                               # object
         return(M)
 }
 
 "cycle" <- function(x){
-    ## Function cycle() takes a list whose elements are lists whose
-    ## elements are vectors (which are disjoint cycles); and returns
-    ## an object of class "cycle".  It nicifies its input (eg removes
-    ## length-1 cycles) before returning it.
-
-    ## A use-case might be
-    ## cycle(list(list(c(1,2,4),c(3,6)),list(c(1,2),c(3,4,5,6,7))))
 
     jj <- unlist(lapply(x, cyclist_valid))
         
     if(all(sapply(jj,isTRUE))){
         x <- lapply(x,nicify_cyclist)    
-        class(x) <- c("permutation", "cycle")  ## NB this is the
-        ## *only* place that
-        ## class "cycle" is
-        ## assigned to an
-        ## object
+        class(x) <- c("permutation", "cycle")  # NB this is the
+                                               # *only* place that
+                                               # class "cycle" is
+                                               # assigned to an
+                                               # object
         return(x)
     } else {
         stop(jj)
@@ -106,13 +87,6 @@ names.word <- function(x){rownames(x)}
     cycle(x[...])
 }
 
-#"[<-.cycle" <- function(x, index, value){
-#    x <- unclass(x)
-#    x[index] <- unclass(as.cycle(value))
-#    browser()
-#    return(cycle(x))  # sic -- not as.cycle(x), because x is a list of cyclists here.
-#}
-
 "c.word" <- function(...){
     a <- list(...)
     if(!all(unlist(lapply(a,is.word)))){
@@ -133,11 +107,6 @@ names.word <- function(x){rownames(x)}
 }
 
 addcols <- function(M,n){
-    
-    ##takes a matrix and adds columns [corresponding to fixed
-    ## elements] so the returned value has 'n' columns.  Used by
-    ## as.word(), so cannot coerce output to class word.
-    
     if(nrow(M)==0){return(matrix(integer(0),0,n))}
     nm <- ncol(M)
     if(n>=nm){
@@ -149,15 +118,6 @@ addcols <- function(M,n){
 
 as.word <- function(x,n=NULL){
     
-    ## This function is the user-friendly way to create a word object
-    ## (compare word(), which is not terribly friendly).  Function
-    ## as.word() does its best to coerce its argument to a word.
-    ## Argument 'n' cannot act to reduce the size of the word, only
-    ## increase it.  If you want to reduce the size, use trim() or
-    ## tidy().  This function does not call word() except directly
-    ## (e.g. it does not call size<-.word(), as this would give a
-    ## recursion).
-
     if(is.word(x)){
         if(missing(n)){
             return(x)
@@ -203,9 +163,9 @@ print.word <- function(x, h=getOption("print_word_as_cycle"), ...){
     }
     if(ncol(x)>0){
         if(is.null(ps)){
-            colnames(x) <- paste("{",seq_len(ncol(x)),"}",sep="")
+            colnames(x) <- paste(seq_len(ncol(x)),sep="")
         } else {
-            colnames(x) <- paste("{",ps[seq_len(ncol(x))],"}",sep="")
+            colnames(x) <- paste(ps[seq_len(ncol(x))],sep="")
         }
     } else {
         cat(" {}")
@@ -218,10 +178,7 @@ print.word <- function(x, h=getOption("print_word_as_cycle"), ...){
     return(invisible(given))
 }
 
-as.cycle <- function(x){   # does its best to coerce to cycle form.
-                                        # Takes character strings and permutation
-                                        # matrices
-
+as.cycle <- function(x){
     if(missing(x)){
         return(id)
     } else if(is.cycle(x)){
@@ -280,10 +237,7 @@ cycle2word <- function(x,n=NULL){  # cycle2word(as.cycle(1:5))
     word(do.call("rbind",lapply(x,cyclist2word_single,n=n)))
 }
 
-cyclist2word_single <- function(cyc,n){     #converts a cyclist to a single
-                                          #permutation (vector):
-                                        #cyclist2word_single(list(c(1,4,3),c(7,8)))
-    
+cyclist2word_single <- function(cyc,n){
     if(length(unlist(cyc))==0){ return(seq_len(n)) }  # checking for the identity
     maxn <-  max(unlist(cyc,recursive=TRUE))
     
@@ -335,12 +289,6 @@ print.cycle <- function(x, give_string=FALSE, ...){  # x is a cycle.  Use case: 
 print_cycle <- function(x){print.cycle(as.cycle(x))}
 
 as.character_cyclist <- function(y,comma=TRUE){
-    
-    ## Use case:
-    ## as.character_cyclist(list(1:4,10:11,20:33)) x is a cyclist;
-    ## as.character_cyclist(list(c(1,5,4),c(2,9)))
-    ## as.character_cyclist(list(c(1,5,4),c(2,9)),comma=TRUE)
-    
     if(length(y)==0){return("()")}
     ps <- getOption("perm_set")
     if(!is.null(ps)){y <- lapply(y,function(x){ps[x]})}
@@ -354,13 +302,6 @@ as.character.cycle <- function(x,...){
 }
 
 standard_cyclist <- function(x,n=NULL){
-
-    ## standard representation as defined by Stanley, p30.  NB
-    ## standard_cyclist() retains length-one cycles (compare
-    ## nicify_cyclist(), which does not).
-
-    ## standard_cyclist(list(c(4, 6), c(7), c(2, 5, 1), c(8, 3)))
-
     xvec <- unlist(x,recursive=TRUE)
     if(is.null(n)){n <- max(xvec)}
     jj <- seq_len(n)
@@ -368,14 +309,6 @@ standard_cyclist <- function(x,n=NULL){
 }
 
 standard <- function(cyc,n=NULL){
-
-    ## Take an object of class cycle and returns a list of cyclists.
-    ## NB does not return a cycle object because cycle() calls
-    ## nicify().
-   
-    ## Use-cases:
-    ## standard(c(as.cycle(1:3),as.cycle(2:3) + as.cycle(6:7)))
-    
     cyc <- as.cycle(cyc)
     xvec <- unlist(cyc,recursive=TRUE)
     if(is.null(n)){n <- max(xvec)}
@@ -398,24 +331,8 @@ fbin_inv <- function(cyc){  # use-case: fbin_inv(as.cycle(rperm(30,9)))
     word(do.call("rbind",lapply(standard(cyc),f)))
 }
 
-nicify_cyclist <- function(x,rm1=TRUE,smallest_first=TRUE){  # needs rm1 argument for
-                                         # standard_cyclist()
+nicify_cyclist <- function(x,rm1=TRUE,smallest_first=TRUE){  # needs rm1 argument for  standard_cyclist()
 
-    ## takes a *cyclist* and puts it in a nice form (does not alter
-    ## the permutation).  Note that nicify_cyclist() removes
-    ## length-one cycles (compare standard_cyclist(), which does not).
-
-    ## NB: nicify_cyclist() is called automatically by cycle().
-    ## Remember that nicify_cyclist() takes a cyclist!
-
-
-    ## use-cases:
-    ## nicify_cyclist(list(c(4, 6), c(7), c(2, 5, 1), c(8, 3)),smallest_first=FALSE,rm1=FALSE)
-    ## nicify_cyclist(list(c(4, 6), c(7), c(2, 5, 1), c(8, 3)),smallest_first=FALSE,rm1=TRUE )
-    ## nicify_cyclist(list(c(4, 6), c(7), c(2, 5, 1), c(8, 3)),smallest_first=TRUE ,rm1=FALSE)
-    ## nicify_cyclist(list(c(4, 6), c(7), c(2, 5, 1), c(8, 3)),smallest_first=TRUE ,rm1=TRUE )
-   
-    
     if(isTRUE(rm1)){  # remove singletons
         x <- remove_length_one(x)  
     }
@@ -432,10 +349,6 @@ nicify_cyclist <- function(x,rm1=TRUE,smallest_first=TRUE){  # needs rm1 argumen
     return(out)
 }
 
-#nicify <- function(x){
-#    cycle(lapply(x,nicify_cyclist))
-#}
-
 remove_length_one <- function(x){
     x[unlist(lapply(x,function(u){length(u)>1}))]
 }
@@ -445,16 +358,6 @@ vec2cyclist_single_cpp <- function(p){
 }
 
 vec2cyclist_single <- function(p){
-
-    ## converts a (vector!) permutation into cycle form and returns a
-    ## *list*.  Just a list! A cyclist! The elements of this list are
-    ## the [disjoint] cycles.  Note the redundancies inherent:
-    ## firstly, because the cycles commute, their order is immaterial
-    ## (and a list is ordered); and secondly, the cycles themselves
-    ## are invariant under cyclic permutation.  Heigh hoo.
-
-    ## test: 793586142 -> (17)(458)(29)(3)
-    ## as.cycle(c(7,9,3,5,8,6,1,4,2))
 
     n <- length(p)  #NB min(p) = 1 (not 0, off-by-one)
     out <- list()
@@ -570,11 +473,7 @@ size.cycle <- function(x){
 
 "size<-" <- function(x, value){ UseMethod("size<-") }
 
-"size<-.word" <- function(x, value){   # trim it down then build
-                                        # it up if necessary;
-                                        # compare addcols() which
-                                        # works purely on
-                                        # matrices.
+"size<-.word" <- function(x, value){   
     stopifnot(is.word(x))
     return(word(addcols(trim(x),value)))
 }
@@ -680,12 +579,6 @@ as.function.permutation <- function(x,...){
     as.function(alist(a=, x[,a]))
 }
            
-#commutator_single <- function(x,y,n){
-#    out <- inverse(x)*inverse(y)*x*y
-#    if(!missing(n)){size(out) <- n}
-#    return(out)
-#}
-
 commutator <- function(x,y){
     n <- max(size(x),size(y))
     jj <- helper(x,y)
