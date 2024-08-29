ccb5ede51e2c0ca0780a72294c70f41f08079ea4
RobinHankin <hankin.robin@gmail.com>
2024-08-29 11:36:04 +0100

    test coverage for rcyc()

---
+++ tests/testthat/test_aag.R
@@ -0,0 +1,16 @@
+test_that("Test suite aag.R; rcyc()", {
+
+  checker1 <- function(x,n) {
+      stopifnot(n > 1)
+      n <- as.integer(n)
+      expect_false(any(is.id(x)))
+      expect_true(all(sapply(shape(x), function(x){identical(x, n)})))
+      expect_true(all(as.function(x)(1) %in% seq(from=2, to=n)))
+  }
+
+  for (i in 1:5) {
+      n <- sample(2:9,1)
+      checker1(rcyc(10,n),n)
+  }
+  
+})
