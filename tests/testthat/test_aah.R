07a26ff035adb2951936c4260973f0de07d9e4cb
RobinHankin <hankin.robin@gmail.com>
2024-08-29 11:37:36 +0100

    test coverage for rgivenshape()

---
+++ tests/testthat/test_aah.R
@@ -0,0 +1,17 @@
+test_that("Test suite aah.R; rgivenshape()", {
+
+  `checker` <- function(x,s) {
+
+      expect_false(any(is.id(x)))
+      expect_true(all(sapply(shape(x, drop = FALSE),function(x){all(sort(x) == sort(s))})))
+  }
+
+  for (i in 1:5) {
+      s <- 2:4
+      checker(rgivenshape(10,s),s)
+      checker(rgivenshape(s),s)
+  }
+
+  
+  
+})
