dba20912f3a1ab44b3bcbdafd6ca8231ea094cee
robin <hankin.robin@gmail.com>
2024-09-01 11:44:26 +0100

    defect in as.function

--- tests/testthat/test_aaj.R
+++ tests/testthat/test_aaj.R
@@ -3,12 +3,14 @@ test_that("Test suite aaj.R; stabilizer()", {
   `checker` <- function(x,s) {
       stab_s <- stabilizer(x,s)
       if(length(stab_s) > 0){
-          for(i in seq_along(s)){
-              expect_true(all(s[i] ^ stab_s %in% s))
+          for(si in s){
+              if(si <= size(stab_s)){
+                  expect_true(all(si ^ stab_s %in% s))
+              }
           }
       }
   }
-
+  
   for (i in 1:3) {
       checker(rperm(20,r=9),sample(9,1,replace=FALSE))
       checker(rperm(20,r=9),sample(9,3,replace=FALSE))
