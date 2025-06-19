4cc21b20b5cc3936ad3923dc4d01610b9d92b910
robin <hankin.robin@gmail.com>
2025-06-19 21:47:09 +0100

    further work on flips

--- R/flip.R
+++ R/flip.R
@@ -3,24 +3,16 @@ is.flip_pair <- function(pair){
     any(apply(edge_facets,1,function(x){all(sort(x) == pair)}))
 }
 
-stopifnot(all(
- is.flip_pair(c(10,46)),
- is.flip_pair(c(98,124)),
-!is.flip_pair(c(10,98)),
-!is.flip_pair(c(11,98))
-))
-
-is.flip_singleperm <- function(p){
-    stopifnot(length(p) == 1)
-    p <- as.cycle(p)
-    if(any(shape(p) != 2)){return(FALSE)}
-    all(sapply(unclass(p)[[1]], is.flip_pair, simplify = TRUE))
-}
-
 is.flip <- function(p){
+    p <- as.cycle(p)
     out <- rep(FALSE,length(p))
     for(i in seq_along(p)){
-        out[i] <- is.flip_singleperm(p[i])
+        out[i] <- (
+            all(shape(p[i]) == 2) &&
+            all(sapply(unclass(p[i])[[1]], is.flip_pair, simplify = TRUE))
+        )
     }
     return(out)
 }
+
+is.superflip <- function(p){is.flip(p) & (sapply(shape(p), length) == 30) }
